99ec0cb09497dbc2611cd5a5e0ab9449507fc7a7
angynk17@gmail.com <capitulo3>
2018-05-28 16:02:58 -0500

    Ajustes cambio de contraseña

--- src/main/java/com/datawarehouse/view/util/PathFiles.java
+++ src/main/java/com/datawarehouse/view/util/PathFiles.java
@@ -3,19 +3,19 @@ package com.datawarehouse.view.util;
 
 public class PathFiles {
 
-// public final static String PATH = "/usr/local/temp/Migracion/";
- public final static String PATH = "C:\\temp\\Datawarehouse\\";
+ public final static String PATH = "/usr/local/temp/Migracion/";
+// public final static String PATH = "C:\\temp\\Datawarehouse\\";
 // public final static String PATH = "C:\\Users\\user\\Documents\\test\\";
 //public final static String PATH = "M:\\temp\\Indicadores\\";
 
-// public static final String PATH_EXPEDICIONES = "/usr/local/temp/Migracion/Expediciones.xls";
- public static final String PATH_EXPEDICIONES = "C:\\temp\\Datawarehouse\\Expediciones.xls";
-// public static final String PATH_BUSES = "/usr/local/temp/Migracion/Buses.xls";
- public static final String PATH_BUSES = "C:\\temp\\Datawarehouse\\Buses.xls";
-// public static final String PATH_DISTRIBUCIONES = "/usr/local/temp/Migracion/Distribuciones.xls";
- public static final String PATH_DISTRIBUCIONES = "C:\\temp\\Datawarehouse\\Distribuciones.xls";
-// public static final String PATH_IPH = "/usr/local/temp/Migracion/TablaHorario.xls";
- public static final String PATH_IPH = "C:\\temp\\Datawarehouse\\TablaHorario.xls";
+ public static final String PATH_EXPEDICIONES = "/usr/local/temp/Migracion/Expediciones.xls";
+// public static final String PATH_EXPEDICIONES = "C:\\temp\\Datawarehouse\\Expediciones.xls";
+ public static final String PATH_BUSES = "/usr/local/temp/Migracion/Buses.xls";
+// public static final String PATH_BUSES = "C:\\temp\\Datawarehouse\\Buses.xls";
+ public static final String PATH_DISTRIBUCIONES = "/usr/local/temp/Migracion/Distribuciones.xls";
+// public static final String PATH_DISTRIBUCIONES = "C:\\temp\\Datawarehouse\\Distribuciones.xls";
+ public static final String PATH_IPH = "/usr/local/temp/Migracion/TablaHorario.xls";
+// public static final String PATH_IPH = "C:\\temp\\Datawarehouse\\TablaHorario.xls";
 
 
     public static final String PATH_REPORTE_TOTAL_KM_VACIOS_OPERADOR = PATH+"Reporte-km-vacios-operador";
